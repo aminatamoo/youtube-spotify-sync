950e47d83fc8fcd03c52dbf6cc50e3e97c11799b
aminatamoo <aminaamoo@gmail.com>
2020-09-16 20:50:40 +0100

    add python project skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='youtube_spotify_sync',
+   version='1.0',
+   description='A module to sync a Youtube Folder with a Spotify Playlist',
+   author='Aminat Amoo',
+   author_email='aminaamoo@gmail.com',
+   packages=find_packages()
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
